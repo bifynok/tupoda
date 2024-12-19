7f8d5ea397667c1878b8020e626cbb1d3caa74be
Kozak Bogdan <bkozak04@gmail.com>
2024-12-20 01:00:23 +0200

    add prog

---
+++ Proga/Proga/Life.cs
@@ -0,0 +1,168 @@
+﻿namespace Proga
+{
+    class Life
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Enter path to file .txt, please");
+                string filename = Console.ReadLine();
+
+                (char[,] map, int generation) = ParseFile(filename);
+
+                char[,] result = Game(map, generation);
+
+                for (int row = 0; row < result.GetLength(0); row++)
+                {
+                    for (int column = 0; column < result.GetLength(1); column++)
+                    {
+                        Console.Write(result[row, column]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString()); ;
+            }
+        }
+
+        static (char[,], int) ParseFile(string filename)
+        {
+            string content = File.ReadAllText(filename);
+
+            int generation = 0;
+            int width = 0;
+            int height = 0;
+
+            int numbersFound = 0;
+
+            foreach (char c in content)
+            {
+                if (numbersFound < 3)
+                {
+                    if (char.IsDigit(c))
+                    {
+                        numbersFound++;
+                        int number = int.Parse(c.ToString());
+
+                        if (numbersFound == 1)
+                            generation = number;
+                        else if (numbersFound == 2)
+                            width = number;
+                        else if (numbersFound == 3)
+                            height = number;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            char[,] map = new char[width, height];
+
+            int row = 0;
+            int column = 0;
+
+            foreach (char c in content)
+            {
+                if (c == '.' || c == 'x')
+                {
+                    map[row, column] = c;
+                    column++;
+
+                    if (column >= height)
+                    {
+                        column = 0;
+                        row++;
+                    }
+                    if (row >= width)
+                    {
+                        break;
+                    }
+                }
+            }
+
+
+            return (map, generation);
+        }
+
+        public static char[,] Game(char[,] map, int generation)
+        {
+            while (generation > 0)
+            {
+                char[,] nextStep = CopyMap(map);
+                for (int row = 0; row < map.GetLength(0); row++)
+                {
+                    for (int column = 0; column < map.GetLength(1); column++)
+                    {
+                        nextStep[row, column] = LifeCheck(map, row, column);
+                    }
+                }
+
+                map = nextStep;
+                generation--;
+            }
+
+            return map;
+        }
+
+        static char LifeCheck(char[,] map, int row, int column)
+        {
+            char res;
+
+            bool isAlive = map[row, column] == 'x';
+
+            int top = row - 1 < 0 ? map.GetLength(0) - 1 : row - 1;
+            int bottom = row + 1 >= map.GetLength(0) ? 0 : row + 1;
+            int left = column - 1 < 0 ? map.GetLength(1) - 1 : column - 1;
+            int right = column + 1 >= map.GetLength(1) ? 0 : column + 1;
+
+            int neightbors = 0;
+
+            if (map[top, left] == 'x')
+                neightbors++;
+            if (map[top, column] == 'x')
+                neightbors++;
+            if (map[top, right] == 'x')
+                neightbors++;
+            if (map[row, right] == 'x')
+                neightbors++;
+            if (map[bottom, right] == 'x')
+                neightbors++;
+            if (map[bottom, column] == 'x')
+                neightbors++;
+            if (map[bottom, left] == 'x')
+                neightbors++;
+            if (map[row, left] == 'x')
+                neightbors++;
+
+            if (isAlive)
+            {
+                res = (neightbors > 1 && neightbors < 4) ? 'x' : '.';
+            }
+            else
+            {
+                res = neightbors == 3 ? 'x' : '.';
+            }
+
+            return res;
+        }
+
+        static char[,] CopyMap(char[,] map)
+        {
+            char[,] copy = new char[map.GetLength(0), map.GetLength(1)];
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    copy[i, j] = map[i, j];
+                }
+            }
+            return copy;
+        }
+    }
+
+}
---
+++ Proga/Test/Test.cs
@@ -0,0 +1,258 @@
+﻿using Proga;
+
+namespace Test
+{
+    class Test
+    {
+        static int Tester()
+        {
+            if (!Case1()) return 1;
+            if (!Case2()) return 1;
+            if (!Case3()) return 1;
+            if (!Case4()) return 1;
+            if (!Case5()) return 1;
+            return 0;
+        }
+
+        static bool Case1()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Case 1");
+            Console.ResetColor();
+
+            char[,] testMap = new char[5, 8] {
+            {'.','.','.','.','.','.','.','.'},
+            {'.','.','.','.','.','.','.','.'},
+            {'.','.','x','x','x','.','.','.'},
+            {'.','.','.','.','.','.','.','.'},
+            {'.','.','.','.','.','.','.','.'}
+        };
+
+            char[,] expectedMap = new char[5, 8]
+            {
+            {'.','.','.','.','.','.','.','.'},
+            {'.','.','.','x','.','.','.','.'},
+            {'.','.','.','x','.','.','.','.'},
+            {'.','.','.','x','.','.','.','.'},
+            {'.','.','.','.','.','.','.','.'}
+            };
+
+            int testGen = 1;
+
+            char[,] resultedMap = Life.Game(testMap, testGen);
+
+            for (int row = 0; row < expectedMap.GetLength(0); row++)
+            {
+                for (int column = 0; column < expectedMap.GetLength(1); column++)
+                {
+                    if (resultedMap[row, column] != expectedMap[row, column])
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Test failed!");
+                        Console.ResetColor();
+                        return false;
+                    }
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Test succseed!");
+            Console.ResetColor();
+            return true;
+        }
+
+        static bool Case2()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Case 2");
+            Console.ResetColor();
+
+            char[,] testMap = new char[4, 3] {
+            {'.','.','x'},
+            {'x','.','.'},
+            {'.','.','x'},
+            {'.','.','.'}
+        };
+
+            char[,] expectedMap = new char[4, 3]
+            {
+            {'.','.','.'},
+            {'.','.','.'},
+            {'.','.','.'},
+            {'.','.','.'}
+            };
+
+            int testGen = 5;
+
+            char[,] resultedMap = Life.Game(testMap, testGen);
+
+            for (int row = 0; row < expectedMap.GetLength(0); row++)
+            {
+                for (int column = 0; column < expectedMap.GetLength(1); column++)
+                {
+                    if (resultedMap[row, column] != expectedMap[row, column])
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Test failed!");
+                        Console.ResetColor();
+                        return false;
+                    }
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Test succseed!");
+            Console.ResetColor();
+            return true;
+        }
+
+        static bool Case3()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Case 3");
+            Console.ResetColor();
+
+            char[,] testMap = new char[6, 6] {
+            {'.','.','.','.','.','.'},
+            {'.','.','.','.','.','.'},
+            {'.','x','.','x','.','x'},
+            {'.','.','x','.','.','x'},
+            {'.','.','.','x','.','.'},
+            {'.','.','.','.','.','.'}
+        };
+
+            char[,] expectedMap = new char[6, 6]
+            {
+            {'.','.','.','.','.','.'},
+            {'.','.','.','x','.','.'},
+            {'.','.','x','.','x','.'},
+            {'.','.','x','.','x','.'},
+            {'.','.','.','x','.','.'},
+            {'.','.','.','.','.','.'}
+            };
+
+            int testGen = 4;
+
+            char[,] resultedMap = Life.Game(testMap, testGen);
+
+            for (int row = 0; row < expectedMap.GetLength(0); row++)
+            {
+                for (int column = 0; column < expectedMap.GetLength(1); column++)
+                {
+                    if (resultedMap[row, column] != expectedMap[row, column])
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Test failed!");
+                        Console.ResetColor();
+                        return false;
+                    }
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Test succseed!");
+            Console.ResetColor();
+            return true;
+        }
+
+        static bool Case4()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Case 4");
+            Console.ResetColor();
+
+            char[,] testMap = new char[5, 8] {
+            {'.','.','.','.','.','.','.','.'},
+            {'.','.','.','.','.','.','.','.'},
+            {'.','.','x','x','.','.','.','.'},
+            {'.','.','x','.','.','.','.','.'},
+            {'.','.','.','.','.','x','x','x'}
+        };
+
+            char[,] expectedMap = new char[5, 8]
+            {
+            {'.','.','.','.','.','.','x','.'},
+            {'.','.','.','.','.','.','.','.'},
+            {'.','.','x','x','.','.','.','.'},
+            {'.','.','x','x','x','.','x','.'},
+            {'.','.','.','.','.','.','x','.'}
+            };
+
+            int testGen = 3;
+
+            char[,] resultedMap = Life.Game(testMap, testGen);
+
+            for (int row = 0; row < expectedMap.GetLength(0); row++)
+            {
+                for (int column = 0; column < expectedMap.GetLength(1); column++)
+                {
+                    if (resultedMap[row, column] != expectedMap[row, column])
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Test failed!");
+                        Console.ResetColor();
+                        return false;
+                    }
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Test succseed!");
+            Console.ResetColor();
+            return true;
+        }
+
+        static bool Case5()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Case 5");
+            Console.ResetColor();
+
+            char[,] testMap = new char[8, 9] {
+            {'.','.','.','.','x','.','.','.','.'},
+            {'.','.','.','.','x','.','.','.','.'},
+            {'.','.','.','.','x','.','.','.','.'},
+            {'.','.','.','.','x','.','.','.','.'},
+            {'.','.','.','.','x','.','.','.','.'},
+            {'.','.','.','.','x','.','.','.','.'},
+            {'.','.','.','.','x','.','.','.','.'},
+            {'.','.','.','.','x','.','.','.','.'}
+        };
+
+            char[,] expectedMap = new char[8, 9]
+            {
+            {'x','.','.','.','.','.','.','.','x'},
+            {'x','.','.','.','.','.','.','.','x'},
+            {'x','.','.','.','.','.','.','.','x'},
+            {'x','.','.','.','.','.','.','.','x'},
+            {'x','.','.','.','.','.','.','.','x'},
+            {'x','.','.','.','.','.','.','.','x'},
+            {'x','.','.','.','.','.','.','.','x'},
+            {'x','.','.','.','.','.','.','.','x'}
+            };
+
+            int testGen = 4;
+
+            char[,] resultedMap = Life.Game(testMap, testGen);
+
+            for (int row = 0; row < expectedMap.GetLength(0); row++)
+            {
+                for (int column = 0; column < expectedMap.GetLength(1); column++)
+                {
+                    if (resultedMap[row, column] != expectedMap[row, column])
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Test failed!");
+                        Console.ResetColor();
+                        return false;
+                    }
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Test succseed!");
+            Console.ResetColor();
+            return true;
+        }
+    }
+}
